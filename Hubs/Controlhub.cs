36f50725c16f681b9bae1d50d5de3a4f07fa48b4
Tobias Johnson <tjohnson@tdj23.com>
2024-11-05 22:28:13 +0100

    rename to player, add id

--- Hubs/Controlhub.cs
+++ Hubs/Controlhub.cs
@@ -4,10 +4,10 @@ namespace umps.Hubs;
 
 public class ControlHub : Hub
 {
-    public async Task SendData(Position data)
+    public async Task SendData(Player player)
     {
         // Broadcast the data to all clients
-        await Clients.All.SendAsync("ReceiveData", data);
-        Console.WriteLine("Received player data: " + data.x + " " + data.y + " " + data.z);
+        await Clients.All.SendAsync("ReceiveData", player);
+        Console.WriteLine("player: " + player.id + " " + player.x + " " + player.y + " " + player.z);
     }
 }
--- Models/Player.cs
+++ Models/Player.cs
@@ -1,5 +1,6 @@
-public class Position
+public class Player
 {
+    public string id { get; set; }
     public double x { get; set; } = 0;
     public double y { get; set; } = 0;
     public double z { get; set; } = 0;
