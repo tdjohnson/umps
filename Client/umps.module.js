aa1c9b3786513b4a5391b9c5bf03523319205cdf
Tobias Johnson <tjohnson@posteo.de>
2024-11-16 13:29:21 +0100

    add client js

---
+++ Client/umps.module.js
@@ -0,0 +1,18 @@
+var playerId = Math.floor(Math.random() * 100).toString();
+ 
+var hub = new signalR.HubConnectionBuilder().withUrl("http://188.245.62.68:8080/controlhub").configureLogging(signalR.LogLevel.Information).build();
+var prePos = -1;
+
+
+hub.start()
+    .then(function () {
+        console.log("Connected to UMPS");
+ 	}).catch(function (err) {
+    console.error("Error connecting to UMPS: ", err);
+	});
+
+hub.on("ReceiveData", function (player) {
+		if(player.id == playerId) return;
+		botBody.position.set(player.x,player.y,player.z);
+		console.log(`player ${player.id} x=${player.x},y=${player.y},z=${player.z}`);
+	});
