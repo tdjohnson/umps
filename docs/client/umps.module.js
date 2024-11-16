17831aa35e8255ad2f8d582b18eee6fb2eda08f9
Tobias Johnson <tjohnson@posteo.de>
2024-11-16 14:18:11 +0100

    add signalr

--- docs/client/umps.module.js
+++ docs/client/umps.module.js
@@ -1,10 +1,14 @@
+import * as SIGNALR from './signalr.js';
+
+// Create a script element
 var script = document.createElement('script');
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/6.0.1/signalr.js";
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/6.0.1/signalr.js;
+// Append the script element to the head of the document
 document.head.appendChild(script);
 
 var playerId = Math.floor(Math.random() * 100).toString();
  
-var hub = new signalR.HubConnectionBuilder().withUrl("http://188.245.62.68:8080/controlhub").configureLogging(signalR.LogLevel.Information).build();
+var hub = new SIGNALR.HubConnectionBuilder().withUrl("http://188.245.62.68:8080/controlhub").configureLogging(signalR.LogLevel.Information).build();
 var prePos = -1;
 
 
