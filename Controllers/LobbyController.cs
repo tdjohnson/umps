8c64b1018a17f55cd0790b687db63a7f4dda5e93
Tobias Johnson <tjohnson@posteo.de>
2025-01-04 21:03:06 +0100

    add player name handling

--- Controllers/LobbyController.cs
+++ Controllers/LobbyController.cs
@@ -1,16 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using umps.Hubs;
+using System.Collections.Concurrent;
 
 namespace umps.Controllers
 {
+
     [ApiController]
     public class LobbyController : ControllerBase
     {
+        private static ConcurrentDictionary<string, string> playerNames = new ConcurrentDictionary<string, string>();
+
         [HttpGet]
         [Route("api/[controller]/GetPlayers")]
         public IActionResult GetPlayers()
         {
             return Ok(ControlHub.ConnectedClients);
+        }
+
+         [HttpPost]
+        [Route("api/[controller]/SetPlayerName")]
+        public IActionResult SetPlayerName([FromQuery] string id, [FromQuery] string name)
+        {
+            playerNames[id] = name;
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/GetPlayerName")]
+        public IActionResult GetPlayerName([FromQuery] string id)
+        {
+            if (playerNames.TryGetValue(id, out var name))
+            {
+                return Ok(name);
+            }
+            return NotFound("Player not found");
         }
     }
 }
