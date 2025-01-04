c80ff61013cca62c2175a34540ad1a23261e3299
Tobias Johnson <tjohnson@posteo.de>
2025-01-04 20:38:07 +0100

    add player.name

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,6 +1,7 @@
 public class Player
 {
     public string id { get; set; }
+    public string name { get; set; }
     public double x { get; set; } = 0;
     public double y { get; set; } = 0;
     public double z { get; set; } = 0;
